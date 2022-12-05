f7b0a3640892337b8bc6b00367c9cda867788520
Chia Wei <chiawei.ong@parcellab.com>
2022-12-05 09:15:32 +0100

    feat: day 5

--- src/day00/mod.rs
+++ src/day00/mod.rs
@@ -6,24 +6,33 @@ pub fn run() {
 	let input_path = format!("src/{module_name}/input.txt");
 	let path = Path::new(&input_path);
 	let raw = fs::read_to_string(path).expect("Could not read input.txt");
-
 	let input = parse_input(&raw);
 
-	let part01_result = part01(input);
-
+	let part01_result = part01(&input);
 	println!("part01 {:?}", part01_result);
 
-	let part02_result = part02(input);
-
+	let part02_result = part02(&input);
 	println!("part02 {:?}", part02_result);
 }
 
-fn parse_input(raw: &String) -> () {
+fn parse_input(raw: &str) -> Vec<()> {
+	let input = raw
+		.lines()
+		.map(|l| {
+			
+		})
+		.collect::<Vec<_>>();
+
+	input
 }
 
-fn part01(input: ()) -> () {}
+fn part01(input: &Vec<()>) -> usize {
+	0
+}
 
-fn part02(input: ()) -> () {}
+fn part02(input: &Vec<()>) -> usize {
+	0
+}
 
 #[cfg(test)]
 mod tests {
@@ -31,27 +40,25 @@ mod tests {
 
 	#[test]
 	fn test_part01() {
-		let raw = String::from("");
+		let raw = concat!(
+			""
+		);
 
-		let input = parse_input(&raw);
+		let input = parse_input(raw);
+		let result = part01(&input);
 
-		assert_eq!(input, ());
-
-		let result = part01(input);
-
-		assert_eq!(result, ());
+		assert_eq!(result, 1);
 	}
 
 	#[test]
 	fn test_part02() {
-		let raw = String::from("");
+		let raw = concat!(
+			""
+		);
 
-		let input = parse_input(&raw);
+		let input = parse_input(raw);
+		let result = part02(&input);
 
-		assert_eq!(input, ());
-
-		let result = part02(input);
-
-		assert_eq!(result, ());
+		assert_eq!(result, 1);
 	}
 }
