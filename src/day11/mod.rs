73c4d45a5e014b020adddaec37f8bac212c4998a
Chia Wei <chiawei.ong@parcellab.com>
2022-12-12 01:16:52 +0100

    refactor

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -46,155 +46,104 @@ pub struct MonkeyParser;
 
 fn part01(input: &str) -> usize {
 	let rounds = 20;
-
-	let mut monkey_inspections: HashMap<usize, usize> = HashMap::new();
-
-	let result = MonkeyParser::parse(Rule::file, input)
-		.expect("could not parse file")
-		.next()
-		.unwrap();
-
-	let mut monkeys = result
-		.into_inner()
-		.filter_map(|monkey| match monkey.as_rule() {
-			Rule::monkey => {
-				let mut monkey_id = 0;
-				let mut items = vec![];
-				let mut operands = vec![];
-				let mut operator = Operator::Add;
-				let mut divisible_by = 1;
-				let mut true_throw_to_id = 0;
-				let mut false_throw_to_id = 0;
-
-				for terms in monkey.into_inner() {
-					match terms.as_rule() {
-						Rule::monkey_id => {
-							monkey_id = terms.as_str().parse::<usize>().unwrap();
-						}
-						Rule::item_list => {
-							for item in terms.into_inner() {
-								match item.as_rule() {
-									Rule::item => {
-										let item = item.as_str().parse::<usize>().unwrap();
-										items.push(item);
-									}
-									_ => unreachable!(),
-								}
-							}
-						}
-						Rule::op_old => {
-							operands.push(Operand::Old);
-						}
-						Rule::op_number => {
-							let num = terms.as_str().parse::<usize>().unwrap();
-							operands.push(Operand::Num(num));
-						}
-						Rule::op => match terms.as_str() {
-							"+" => operator = Operator::Add,
-							"*" => operator = Operator::Mult,
-							_ => unreachable!(),
-						},
-						Rule::div_number => {
-							divisible_by = terms.as_str().parse::<usize>().unwrap();
-						}
-						Rule::true_throw_to_id => {
-							true_throw_to_id = terms.as_str().parse::<usize>().unwrap();
-						}
-						Rule::false_throw_to_id => {
-							false_throw_to_id = terms.as_str().parse::<usize>().unwrap();
-						}
-						_ => {}
-					}
-				}
-
-				let monkey = Monkey {
-					id: monkey_id,
-					items,
-					operands,
-					operator,
-					divisible_by,
-					true_throw_to_id,
-					false_throw_to_id,
-				};
-
-				Some(monkey)
-			}
-			_ => None,
-		})
-		.collect::<Vec<_>>();
-
-	for round in 1..=rounds {
-		for mi in 0..monkeys.len() {
-			let mut item_changes = HashMap::new();
-			let m = monkeys.get(mi).unwrap();
-			item_changes.insert(m.id, vec![]);
-
-			m.items.iter().for_each(|i| {
-				monkey_inspections
-					.entry(mi)
-					.and_modify(|i| *i += 1)
-					.or_insert(1);
-
-				let first = match m.operands[0] {
-					Operand::Old => i.clone(),
-					Operand::Num(n) => n.clone(),
-				};
-				let second = match m.operands[1] {
-					Operand::Old => i.clone(),
-					Operand::Num(n) => n.clone(),
-				};
-				let result = match m.operator {
-					Operator::Add => first + second,
-					Operator::Mult => first * second,
-				};
-				let result = result / 3;
-				if result % m.divisible_by == 0 {
-					item_changes
-						.entry(m.true_throw_to_id)
-						.and_modify(|i| i.push(result))
-						.or_insert_with(|| {
-							let mut old_items = monkeys[m.true_throw_to_id].items.clone();
-							old_items.push(result);
-							old_items
-						});
-				} else {
-					item_changes
-						.entry(m.false_throw_to_id)
-						.and_modify(|i| i.push(result))
-						.or_insert_with(|| {
-							let mut old_items = monkeys[m.false_throw_to_id].items.clone();
-							old_items.push(result);
-							old_items
-						});
-				}
-			});
-
-			for (id, items) in item_changes {
-				monkeys.get_mut(id).unwrap().items = items;
-			}
-		}
-	}
+	let monkeys = parse_input(input);
+	let manage_worry = |r: usize| -> usize { r / 3 };
+	let monkey_inspections = get_inspections_after_rounds(rounds, monkeys, manage_worry);
 
 	let mut inspections_sorted = monkey_inspections.values().collect::<Vec<_>>();
 	inspections_sorted.sort();
 	inspections_sorted.reverse();
 
 	let monkey_business = inspections_sorted[0] * inspections_sorted[1];
-
 	monkey_business
 }
 
 fn part02(input: &str) -> usize {
 	let rounds = 10_000;
+	let monkeys = parse_input(input);
+	let common_divisible = monkeys
+		.iter()
+		.map(|m| m.divisible_by)
+		.reduce(|acc, d| acc * d)
+		.unwrap();
+	let manage_worry = |r: usize| -> usize { r % common_divisible };
+	let monkey_inspections = get_inspections_after_rounds(rounds, monkeys, manage_worry);
 
+	let mut inspections_sorted = monkey_inspections.values().collect::<Vec<_>>();
+	inspections_sorted.sort();
+	inspections_sorted.reverse();
+
+	let monkey_business = inspections_sorted[0] * inspections_sorted[1];
+	monkey_business
+}
+
+fn get_inspections_after_rounds<F: Fn(usize) -> usize>(
+	rounds: i32,
+	mut monkeys: Vec<Monkey>,
+	manage_worry: F,
+) -> HashMap<usize, usize> {
 	let mut monkey_inspections: HashMap<usize, usize> = HashMap::new();
+	for _ in 1..=rounds {
+		for mi in 0..monkeys.len() {
+			let mut item_changes = HashMap::new();
+			let m = monkeys.get(mi).unwrap();
+			item_changes.insert(m.id, vec![]);
 
+			m.items.iter().for_each(|i| {
+				monkey_inspections
+					.entry(mi)
+					.and_modify(|i| *i += 1)
+					.or_insert(1);
+
+				let first = match m.operands[0] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let second = match m.operands[1] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let result = match m.operator {
+					Operator::Add => first + second,
+					Operator::Mult => first * second,
+				};
+				let result = manage_worry(result);
+				if result % m.divisible_by == 0 {
+					item_changes
+						.entry(m.true_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.true_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				} else {
+					item_changes
+						.entry(m.false_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.false_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				}
+			});
+
+			for (id, items) in item_changes {
+				monkeys.get_mut(id).unwrap().items = items;
+			}
+		}
+	}
+	monkey_inspections
+}
+
+fn parse_input(input: &str) -> Vec<Monkey> {
 	let result = MonkeyParser::parse(Rule::file, input)
 		.expect("could not parse file")
 		.next()
 		.unwrap();
 
-	let mut monkeys = result
+	result
 		.into_inner()
 		.filter_map(|monkey| match monkey.as_rule() {
 			Rule::monkey => {
@@ -261,73 +210,7 @@ fn part02(input: &str) -> usize {
 			}
 			_ => None,
 		})
-		.collect::<Vec<_>>();
-
-	let common_divisible = monkeys
-		.iter()
-		.map(|m| m.divisible_by)
-		.reduce(|acc, d| acc * d)
-		.unwrap();
-
-	for round in 1..=rounds {
-		for mi in 0..monkeys.len() {
-			let mut item_changes = HashMap::new();
-			let m = monkeys.get(mi).unwrap();
-			item_changes.insert(m.id, vec![]);
-
-			m.items.iter().for_each(|i| {
-				monkey_inspections
-					.entry(mi)
-					.and_modify(|i| *i += 1)
-					.or_insert(1);
-
-				let first = match m.operands[0] {
-					Operand::Old => i.clone(),
-					Operand::Num(n) => n.clone(),
-				};
-				let second = match m.operands[1] {
-					Operand::Old => i.clone(),
-					Operand::Num(n) => n.clone(),
-				};
-				let result = match m.operator {
-					Operator::Add => first + second,
-					Operator::Mult => first * second,
-				};
-				let result = result % common_divisible;
-				if result % m.divisible_by == 0 {
-					item_changes
-						.entry(m.true_throw_to_id)
-						.and_modify(|i| i.push(result))
-						.or_insert_with(|| {
-							let mut old_items = monkeys[m.true_throw_to_id].items.clone();
-							old_items.push(result);
-							old_items
-						});
-				} else {
-					item_changes
-						.entry(m.false_throw_to_id)
-						.and_modify(|i| i.push(result))
-						.or_insert_with(|| {
-							let mut old_items = monkeys[m.false_throw_to_id].items.clone();
-							old_items.push(result);
-							old_items
-						});
-				}
-			});
-
-			for (id, items) in item_changes {
-				monkeys.get_mut(id).unwrap().items = items;
-			}
-		}
-	}
-
-	let mut inspections_sorted = monkey_inspections.values().collect::<Vec<_>>();
-	inspections_sorted.sort();
-	inspections_sorted.reverse();
-
-	let monkey_business = inspections_sorted[0] * inspections_sorted[1];
-
-	monkey_business
+		.collect::<Vec<_>>()
 }
 
 #[cfg(test)]
