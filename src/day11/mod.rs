64d26f583bc12e63099eb97d0a7f51ea9dff77a6
Chia Wei <chiawei.ong@parcellab.com>
2022-12-12 01:00:16 +0100

    part 2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -45,89 +45,289 @@ struct Monkey {
 pub struct MonkeyParser;
 
 fn part01(input: &str) -> usize {
+	let rounds = 20;
+
+	let mut monkey_inspections: HashMap<usize, usize> = HashMap::new();
+
 	let result = MonkeyParser::parse(Rule::file, input)
 		.expect("could not parse file")
 		.next()
 		.unwrap();
 
-	let monkeys = result
+	let mut monkeys = result
 		.into_inner()
-		.map(|monkey| {
-			let mut monkey_id = 0;
-			let mut items = vec![];
-			let mut operands = vec![];
-			let mut operator = Operator::Add;
-			let mut divisible_by = 1;
-			let mut true_throw_to_id = 0;
-			let mut false_throw_to_id = 0;
+		.filter_map(|monkey| match monkey.as_rule() {
+			Rule::monkey => {
+				let mut monkey_id = 0;
+				let mut items = vec![];
+				let mut operands = vec![];
+				let mut operator = Operator::Add;
+				let mut divisible_by = 1;
+				let mut true_throw_to_id = 0;
+				let mut false_throw_to_id = 0;
 
-			match monkey.as_rule() {
-				Rule::monkey => {
-					for terms in monkey.into_inner() {
-						match terms.as_rule() {
-							Rule::monkey_id => {
-								monkey_id = terms.as_str().parse::<usize>().unwrap();
-							}
-							Rule::item_list => {
-								for item in terms.into_inner() {
-									match item.as_rule() {
-										Rule::item => {
-											let item = item.as_str().parse::<usize>().unwrap();
-											items.push(item);
-										}
-										_ => unreachable!(),
+				for terms in monkey.into_inner() {
+					match terms.as_rule() {
+						Rule::monkey_id => {
+							monkey_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::item_list => {
+							for item in terms.into_inner() {
+								match item.as_rule() {
+									Rule::item => {
+										let item = item.as_str().parse::<usize>().unwrap();
+										items.push(item);
 									}
+									_ => unreachable!(),
 								}
 							}
-							Rule::op_old => {
-								operands.push(Operand::Old);
-							}
-							Rule::op_number => {
-								let num = terms.as_str().parse::<usize>().unwrap();
-								operands.push(Operand::Num(num));
-							}
-							Rule::op => match terms.as_str() {
-								"+" => operator = Operator::Add,
-								"*" => operator = Operator::Mult,
-								_ => unreachable!(),
-							},
-							Rule::div_number => {
-								divisible_by = terms.as_str().parse::<usize>().unwrap();
-							}
-							Rule::true_throw_to_id => {
-								true_throw_to_id = terms.as_str().parse::<usize>().unwrap();
-							}
-							Rule::false_throw_to_id => {
-								false_throw_to_id = terms.as_str().parse::<usize>().unwrap();
-							}
-							_ => {}
 						}
+						Rule::op_old => {
+							operands.push(Operand::Old);
+						}
+						Rule::op_number => {
+							let num = terms.as_str().parse::<usize>().unwrap();
+							operands.push(Operand::Num(num));
+						}
+						Rule::op => match terms.as_str() {
+							"+" => operator = Operator::Add,
+							"*" => operator = Operator::Mult,
+							_ => unreachable!(),
+						},
+						Rule::div_number => {
+							divisible_by = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::true_throw_to_id => {
+							true_throw_to_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::false_throw_to_id => {
+							false_throw_to_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						_ => {}
 					}
 				}
-				_ => {}
+
+				let monkey = Monkey {
+					id: monkey_id,
+					items,
+					operands,
+					operator,
+					divisible_by,
+					true_throw_to_id,
+					false_throw_to_id,
+				};
+
+				Some(monkey)
 			}
-
-			let monkey = Monkey {
-				id: monkey_id,
-				items,
-				operands,
-				operator,
-				divisible_by,
-				true_throw_to_id,
-				false_throw_to_id,
-			};
-
-			monkey
+			_ => None,
 		})
 		.collect::<Vec<_>>();
 
-	println!("{:?}", monkeys);
+	for round in 1..=rounds {
+		for mi in 0..monkeys.len() {
+			let mut item_changes = HashMap::new();
+			let m = monkeys.get(mi).unwrap();
+			item_changes.insert(m.id, vec![]);
 
-	0
+			m.items.iter().for_each(|i| {
+				monkey_inspections
+					.entry(mi)
+					.and_modify(|i| *i += 1)
+					.or_insert(1);
+
+				let first = match m.operands[0] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let second = match m.operands[1] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let result = match m.operator {
+					Operator::Add => first + second,
+					Operator::Mult => first * second,
+				};
+				let result = result / 3;
+				if result % m.divisible_by == 0 {
+					item_changes
+						.entry(m.true_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.true_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				} else {
+					item_changes
+						.entry(m.false_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.false_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				}
+			});
+
+			for (id, items) in item_changes {
+				monkeys.get_mut(id).unwrap().items = items;
+			}
+		}
+	}
+
+	let mut inspections_sorted = monkey_inspections.values().collect::<Vec<_>>();
+	inspections_sorted.sort();
+	inspections_sorted.reverse();
+
+	let monkey_business = inspections_sorted[0] * inspections_sorted[1];
+
+	monkey_business
 }
 
 fn part02(input: &str) -> usize {
-	0
+	let rounds = 10_000;
+
+	let mut monkey_inspections: HashMap<usize, usize> = HashMap::new();
+
+	let result = MonkeyParser::parse(Rule::file, input)
+		.expect("could not parse file")
+		.next()
+		.unwrap();
+
+	let mut monkeys = result
+		.into_inner()
+		.filter_map(|monkey| match monkey.as_rule() {
+			Rule::monkey => {
+				let mut monkey_id = 0;
+				let mut items = vec![];
+				let mut operands = vec![];
+				let mut operator = Operator::Add;
+				let mut divisible_by = 1;
+				let mut true_throw_to_id = 0;
+				let mut false_throw_to_id = 0;
+
+				for terms in monkey.into_inner() {
+					match terms.as_rule() {
+						Rule::monkey_id => {
+							monkey_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::item_list => {
+							for item in terms.into_inner() {
+								match item.as_rule() {
+									Rule::item => {
+										let item = item.as_str().parse::<usize>().unwrap();
+										items.push(item);
+									}
+									_ => unreachable!(),
+								}
+							}
+						}
+						Rule::op_old => {
+							operands.push(Operand::Old);
+						}
+						Rule::op_number => {
+							let num = terms.as_str().parse::<usize>().unwrap();
+							operands.push(Operand::Num(num));
+						}
+						Rule::op => match terms.as_str() {
+							"+" => operator = Operator::Add,
+							"*" => operator = Operator::Mult,
+							_ => unreachable!(),
+						},
+						Rule::div_number => {
+							divisible_by = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::true_throw_to_id => {
+							true_throw_to_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						Rule::false_throw_to_id => {
+							false_throw_to_id = terms.as_str().parse::<usize>().unwrap();
+						}
+						_ => {}
+					}
+				}
+
+				let monkey = Monkey {
+					id: monkey_id,
+					items,
+					operands,
+					operator,
+					divisible_by,
+					true_throw_to_id,
+					false_throw_to_id,
+				};
+
+				Some(monkey)
+			}
+			_ => None,
+		})
+		.collect::<Vec<_>>();
+
+	let common_divisible = monkeys
+		.iter()
+		.map(|m| m.divisible_by)
+		.reduce(|acc, d| acc * d)
+		.unwrap();
+
+	for round in 1..=rounds {
+		for mi in 0..monkeys.len() {
+			let mut item_changes = HashMap::new();
+			let m = monkeys.get(mi).unwrap();
+			item_changes.insert(m.id, vec![]);
+
+			m.items.iter().for_each(|i| {
+				monkey_inspections
+					.entry(mi)
+					.and_modify(|i| *i += 1)
+					.or_insert(1);
+
+				let first = match m.operands[0] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let second = match m.operands[1] {
+					Operand::Old => i.clone(),
+					Operand::Num(n) => n.clone(),
+				};
+				let result = match m.operator {
+					Operator::Add => first + second,
+					Operator::Mult => first * second,
+				};
+				let result = result % common_divisible;
+				if result % m.divisible_by == 0 {
+					item_changes
+						.entry(m.true_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.true_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				} else {
+					item_changes
+						.entry(m.false_throw_to_id)
+						.and_modify(|i| i.push(result))
+						.or_insert_with(|| {
+							let mut old_items = monkeys[m.false_throw_to_id].items.clone();
+							old_items.push(result);
+							old_items
+						});
+				}
+			});
+
+			for (id, items) in item_changes {
+				monkeys.get_mut(id).unwrap().items = items;
+			}
+		}
+	}
+
+	let mut inspections_sorted = monkey_inspections.values().collect::<Vec<_>>();
+	inspections_sorted.sort();
+	inspections_sorted.reverse();
+
+	let monkey_business = inspections_sorted[0] * inspections_sorted[1];
+
+	monkey_business
 }
 
 #[cfg(test)]
@@ -166,13 +366,12 @@ Monkey 3:
 	#[test]
 	fn test_part01() {
 		let result = part01(INPUT);
-		assert_eq!(result, 1);
+		assert_eq!(result, 10605);
 	}
 
 	#[test]
 	fn test_part02() {
-		let input = concat!();
-		let result = part02(input);
-		assert_eq!(result, 1);
+		let result = part02(INPUT);
+		assert_eq!(result, 2713310158);
 	}
 }
